196865c3a9a4a7f5e992cae3fb6411657dd9edb7
AsierLambarri <asierlambarri@gmail.com>
2026-05-21 19:10:11 +0200

    10.4+11.4: public API in __init__.py; 11.1: run_pipeline.py CLI; 11.2: analysis scripts (analyze, SFR, radii, movie)

--- src/roadrunner/__init__.py
+++ src/roadrunner/__init__.py
@@ -1,1 +1,39 @@
 from . import _accme
+
+from .pipeline import run_accretion_history, RunConfig, \
+    SnapshotProcessor, AccretionPipeline
+from .readers.merger_tree import MergerTreeReaderCSV
+from .readers.snapshot import SnapshotReader
+from .readers.equivalence import EquivalenceTable
+from .mixture.weighted_gmm import WeightedGaussianMixture
+from .mixture.bayesian_gmm import WeightedBayesianGaussianMixture
+from .mixture.coresets import GaussianCoreset
+from .postprocessing.tracking.birth import BirthTracker
+from .postprocessing.tracking.assembly import AssemblyTracker
+from .postprocessing.mixing import compute_riley_criterion
+from .postprocessing.properties import compute_galaxy_properties
+from .clustering.assignment.gmm import GMMAssigner
+from .physics.halo_model import HaloModel
+from .physics.potentials import get_potential, KeplerPotential, NFWPotential
+
+__all__ = [
+    "run_accretion_history",
+    "RunConfig",
+    "SnapshotProcessor",
+    "AccretionPipeline",
+    "MergerTreeReaderCSV",
+    "SnapshotReader",
+    "EquivalenceTable",
+    "WeightedGaussianMixture",
+    "WeightedBayesianGaussianMixture",
+    "GaussianCoreset",
+    "BirthTracker",
+    "AssemblyTracker",
+    "compute_riley_criterion",
+    "compute_galaxy_properties",
+    "GMMAssigner",
+    "HaloModel",
+    "get_potential",
+    "KeplerPotential",
+    "NFWPotential",
+]
